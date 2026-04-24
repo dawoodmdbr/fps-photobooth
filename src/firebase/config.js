13a64d3d9510a1862dd745a0842ea6644d89a934
Mian Dawood Bin Rafay <dawoodbinrafaydbr@gmail.com>
2026-04-25 01:36:17 +0500

    update config.js and added gitignore

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,19 +1,23 @@
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 // Replace with your Firebase project config
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyCsQmrGr2oVm0txuAJhNXAYY4B6B8CkUkE",
+  authDomain: "fps-photobooth.firebaseapp.com",
+  projectId: "fps-photobooth",
+  storageBucket: "fps-photobooth.firebasestorage.app",
+  messagingSenderId: "1022941141529",
+  appId: "1:1022941141529:web:4a77eddb01e5f142d16888",
+  measurementId: "G-72SHTCSYYF"
 };
 
+
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
